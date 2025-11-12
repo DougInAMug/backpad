4a133cb5db4eab5fec5e483e695ab06e7a865cb5
douginamug <dmhwebb@gmail.com>
2025-11-12 17:59:43 +0100

    load data for users other than current

--- src/routes/(auth)/+layout.server.ts
+++ src/routes/(auth)/+layout.server.ts
@@ -1,15 +1,20 @@
 import { redirect } from "@sveltejs/kit";
 import { auth } from "$lib/auth";
 import type { LayoutServerLoad } from "./$types";
+import prisma from "$lib/prisma";
 
 export const load: LayoutServerLoad = async ({ request }) => {
   const session = await auth.api.getSession({ headers: request.headers });
+
+  const allUsers = await prisma.user.findMany();
+  const allUsersMinusCurrent = allUsers.filter((i) => i.id != session?.user.id);
 
   if (!session) {
     throw redirect(302, "/logIn");
   }
   return {
     user: session.user,
+    allUsersMinusCurrent,
   };
 };
 
--- src/routes/(auth)/profile/+page.svelte
+++ src/routes/(auth)/profile/+page.svelte
@@ -2,25 +2,31 @@
   import { authClient } from "$lib/auth-client";
   import { goto } from "$app/navigation";
   import { page } from "$app/state";
-  import type { PageData } from "./$types";
+  import type { PageData, PageProps } from "./$types";
 
   const { user } = page.data as PageData;
+  let { data }: PageProps = $props();
 </script>
 
-{#if user}
-  <h1>Hello {user.name}</h1>
+<h1>Hello {user.name}</h1>
 
-  <button
-    onclick={async () => {
-      await authClient.signOut({
-        fetchOptions: {
-          onSuccess: () => {
-            goto("/");
-          },
+<button
+  onclick={async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          goto("/");
         },
-      });
-    }}
-  >
-    Log out
-  </button>
-{/if}
+      },
+    });
+  }}
+>
+  Log out
+</button>
+
+{#each data.allUsersMinusCurrent as u}
+  <details>
+    <summary>{u.name}</summary>
+    {u.backpad}
+  </details>
+{/each}
